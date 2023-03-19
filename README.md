c429f1cbd17d8448b45c96fc3f151974300e6436
wali55 <walisantunu@gmail.com>
2023-03-20 00:10:06 +0600

    client site finished

---
+++ README.md
@@ -0,0 +1,1 @@
+## View full site [https://mevn-web-app.onrender.com](https://mevn-web-app.onrender.com).
---
+++ src/router/index.js
@@ -0,0 +1,48 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import AddPost from '../views/AddPost.vue';
+import PostPage from '../views/PostPage.vue';
+import EditPost from '../views/EditPost.vue';
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView,
+  },
+  {
+    path: '/add-post',
+    name: 'add-post',
+    component: AddPost,
+  },
+  {
+    path: '/post/:id',
+    name: 'post',
+    component: PostPage,
+  },
+  {
+    path: '/edit-post/:id',
+    name: 'edit-post',
+    component: EditPost,
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+  },
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
